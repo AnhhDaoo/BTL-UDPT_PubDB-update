7911c0554df2a3af02d5c2d73f882e0b14bcc1f9
AnhhDaoo <anhdao19623@gmail.com>
2025-02-19 01:18:31 +0700

    new function: add thread_lock into core.py and test_multithreaded.py

---
+++ tests/test_multithreaded.py
@@ -0,0 +1,117 @@
+"""
+    Tests to perform reads/writes to the database concurrently i.e.
+    from multiple threads.
+"""
+
+import logging
+import os
+from threading import Thread
+import json
+import sys
+
+import pytest
+
+from pupdb.core import PupDB
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s | %(process)d | %(levelname)s | %(message)s'
+)
+
+
+TEST_DB_PATH = 'testdb.json'
+TEST_DB_LOCK_PATH = '{}.lock'.format(TEST_DB_PATH)
+
+
+class PupDBWriterThread(Thread):
+    """
+        This class represents an instance of a thread that writes
+        to the database.
+    """
+
+    def __init__(self, data_range, database=None):
+        """ Instance Initialization """
+
+        super(PupDBWriterThread, self).__init__()
+        self.data_range = data_range
+        if database is None:
+            self.database = PupDB(TEST_DB_PATH)
+        else:
+            self.database = database
+
+    def run(self):
+        """ Method performs write to the database."""
+
+        for i in self.data_range:
+            self.database.set(i, i)
+
+
+@pytest.fixture(autouse=True)
+def run_around_tests():
+    """ Function is invoked around each test run. """
+
+    logging.debug('Test started.')
+    yield
+    logging.debug('Test ended.')
+
+    if os.path.exists(TEST_DB_PATH):
+        os.remove(TEST_DB_PATH)
+
+    if os.path.exists(TEST_DB_LOCK_PATH):
+        os.remove(TEST_DB_LOCK_PATH)
+
+
+def test_mt_get_and_set_mi():
+    """
+        Tests the get() and set() methods of PupDB,
+        in a multi-threaded scenario, where each thread has it's own
+        instance (object) of PupDB.
+    """
+
+    data_ranges = [
+        range(1, 50),
+        range(50, 100),
+        range(100, 150),
+        range(150, 201)
+    ]
+    writers = []
+
+    # Write from multiple threads.
+    for data_range in data_ranges:
+        writer = PupDBWriterThread(data_range)
+        writers.append(writer)
+        writer.start()
+
+    for writer in writers:
+        writer.join()
+
+    # Verify if all keys have been written properly.
+    database = PupDB(TEST_DB_PATH)
+    assert len(database) == 200
+
+def test_mt_get_and_set_si():
+    """
+        Tests the get() and set() methods of PupDB,
+        in a multi-threaded scenario, where all threads share a
+        single instance (object) of PupDB.
+    """
+    data_ranges = [
+        range(1, 50),
+        range(50, 100),
+        range(100, 150),
+        range(150, 201)
+    ]
+    writers = []
+    database = PupDB(TEST_DB_PATH)
+
+    # Write from multiple threads using a shared instance.
+    for data_range in data_ranges:
+        writer = PupDBWriterThread(data_range, database)
+        writers.append(writer)
+        writer.start()
+
+    for writer in writers:
+        writer.join()
+
+    # Verify if all keys have been written properly.
+    assert len(database) == 200
