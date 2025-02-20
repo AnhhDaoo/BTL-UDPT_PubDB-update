8e1bdc1202aa4919ef909993adbbc7c6d461c06d
AnhhDaoo <anhdao19623@gmail.com>
2025-02-21 00:50:48 +0700

    ket hop process_lock va thread_lock, dam bao an toan moi truong da tien trinh/da luong

--- pupdb/core.py
+++ pupdb/core.py
@@ -66,87 +66,146 @@ class PupDB(object):
                 return True
 
 
-    def set(self, key, val):
-    # Giữ khóa file cho toàn bộ quá trình read-modify-write.
-        with self.process_lock:
-            try:
-                # Đọc database (không cần _get_database() vì đã có khóa)
-                with open(self.db_file_path, 'r') as db_file:
-                    database = json.load(db_file)
-                # Cập nhật dữ liệu
-                database[key] = val
-                # Ghi lại database
-                with open(self.db_file_path, 'w') as db_file:
-                    json.dump(database, db_file)
-            except Exception:
-                logging.error('Error while writing to DB: %s', traceback.format_exc())
-                return False
-            return True
+    # def set(self, key, val):
+    # # Giữ khóa file cho toàn bộ quá trình read-modify-write.
+    #     with self.process_lock:
+    #         try:
+    #             # Đọc database (không cần _get_database() vì đã có khóa)
+    #             with open(self.db_file_path, 'r') as db_file:
+    #                 database = json.load(db_file)
+    #             # Cập nhật dữ liệu
+    #             database[key] = val
+    #             # Ghi lại database
+    #             with open(self.db_file_path, 'w') as db_file:
+    #                 json.dump(database, db_file)
+    #         except Exception:
+    #             logging.error('Error while writing to DB: %s', traceback.format_exc())
+    #             return False
+    #         return True
 
+
+    def set(self, key, val):
+        with self._thread_lock:
+            with self.process_lock:
+                try:
+                    with open(self.db_file_path, 'r') as db_file:
+                        database = json.load(db_file)
+                    database[key] = val
+                    with open(self.db_file_path, 'w') as db_file:
+                        json.dump(database, db_file)
+                except Exception:
+                    logging.error('Error while writing to DB: %s', traceback.format_exc())
+                    return False
+        return True
+
+
+    # def get(self, key):
+    #     """
+    #         Gets the value of a key from the database.
+    #         Returns None if the key is not found in the database.
+    #     """
+
+    #     key = str(key)
+    #     database = self._get_database()
+    #     return database.get(key, None)
 
     def get(self, key):
-        """
-            Gets the value of a key from the database.
-            Returns None if the key is not found in the database.
-        """
-
         key = str(key)
-        database = self._get_database()
+        with self._thread_lock:
+            database = self._get_database()
         return database.get(key, None)
 
         
+    # def remove(self, key):
+    #     """
+    #     Removes a key from the database.
+    #     """
+    #     key = str(key)
+    #     with self.process_lock:
+    #         # Đọc database
+    #         with open(self.db_file_path, 'r') as db_file:
+    #             database = json.load(db_file)
+    #         # Nếu key không tồn tại, raise KeyError ngay
+    #         if key not in database:
+    #             raise KeyError('Non-existent Key {} in database'.format(key))
+    #         # Xoá key
+    #         del database[key]
+    #         # Ghi lại database
+    #         with open(self.db_file_path, 'w') as db_file:
+    #             json.dump(database, db_file)
+    #     return True
+
     def remove(self, key):
-        """
-        Removes a key from the database.
-        """
         key = str(key)
-        with self.process_lock:
-            # Đọc database
-            with open(self.db_file_path, 'r') as db_file:
-                database = json.load(db_file)
-            # Nếu key không tồn tại, raise KeyError ngay
-            if key not in database:
-                raise KeyError('Non-existent Key {} in database'.format(key))
-            # Xoá key
-            del database[key]
-            # Ghi lại database
-            with open(self.db_file_path, 'w') as db_file:
-                json.dump(database, db_file)
+        with self._thread_lock:
+            with self.process_lock:
+                with open(self.db_file_path, 'r') as db_file:
+                    database = json.load(db_file)
+                if key not in database:
+                    raise KeyError('Non-existent Key {} in database'.format(key))
+                del database[key]
+                with open(self.db_file_path, 'w') as db_file:
+                    json.dump(database, db_file)
         return True
 
- 
         
+    # def keys(self):
+    #     """
+    #         Returns a list (py27) or iterator (py3) of all the keys
+    #         in the database.
+    #     """
+
+    #     return self._get_database().keys()
+
     def keys(self):
-        """
-            Returns a list (py27) or iterator (py3) of all the keys
-            in the database.
-        """
+        with self._thread_lock:
+            return self._get_database().keys()
 
-        return self._get_database().keys()
 
+    # def values(self):
+    #     """
+    #         Returns a list (py27) or iterator (py3) of all the values
+    #         in the database.
+    #     """
+
+    #     return self._get_database().values()
+    
     def values(self):
-        """
-            Returns a list (py27) or iterator (py3) of all the values
-            in the database.
-        """
+        with self._thread_lock:
+            return self._get_database().values()
 
-        return self._get_database().values()
 
+    # def items(self):
+    #     """
+    #         Returns a list (py27) or iterator (py3) of all the items i.e.
+    #         (key, val) pairs in the database.
+    #     """
+
+    #     return self._get_database().items()
+    
     def items(self):
-        """
-            Returns a list (py27) or iterator (py3) of all the items i.e.
-            (key, val) pairs in the database.
-        """
+        with self._thread_lock:
+            return self._get_database().items()
 
-        return self._get_database().items()
 
+    # def dumps(self):
+    #     """ Returns a string dump of the entire database sorted by key. """
+
+    #     return json.dumps(self._get_database(), sort_keys=True)
+    
     def dumps(self):
-        """ Returns a string dump of the entire database sorted by key. """
+        with self._thread_lock:
+            return json.dumps(self._get_database(), sort_keys=True)
 
-        return json.dumps(self._get_database(), sort_keys=True)
 
+    # def truncate_db(self):
+    #     """ Truncates the entire database (makes it empty). """
+
+    #     self._flush_database({})
+    #     return True
+    
     def truncate_db(self):
-        """ Truncates the entire database (makes it empty). """
-
-        self._flush_database({})
+        with self._thread_lock:
+            self._flush_database({})
         return True
+
